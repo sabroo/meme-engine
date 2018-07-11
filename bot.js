bbc572cbed3445b25053525e0b67c35d7101f87a
LadyLu <41097277+LadyLu@users.noreply.github.com>
2018-07-11 01:15:39 -0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content.includes("knife")) {
+    	message.channel.send("Ah, did someone say knife? I do rather appreciate knives.");
+  	} else
+
+  if (message.content.includes("Inari")) {
+    message.channel.send("May that fox burn in her own flames.");
+  } else
+
+  if (message.content.includes("Leo")) {
+    message.channel.send("The lion? A mere kitten, in my opinion.");
+  } else
+  
+   if (message.content.includes("Echo")) {
+    message.channel.send("She is rather... Entertaining.");
+  }
+     
+
+ 
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);

792af535cd1baf650cab8a1a5ce30bc6d615b383
LadyLu <41097277+LadyLu@users.noreply.github.com>
2018-07-11 01:27:12 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,9 +6,13 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content.includes("knife")) {
-    	message.channel.send("Ah, did someone say knife? I do rather appreciate knives.");
+    if (message.content.startsWith("Chikelu")) {
+    	message.channel.send("Time to strangle my prey~");
   	} else
+
+  if (message.content.includes("knife")) {
+    message.channel.send("Ah, did someone say knife? I do rather appreciate knives.");
+  } else
 
   if (message.content.includes("Inari")) {
     message.channel.send("May that fox burn in her own flames.");
